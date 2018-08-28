a24f43d3b7dcedda63dc9c88390c32ee4c1c4386
DESKTOP-7G8T85V\mc <manfung98@yahoo.com>
2018-08-28 15:56:24 +0100

    Making the Article URL as the primary key in database to avoid duplicates

--- app/src/main/java/com/tephra/mc/latestnews/data/model/Article.kt
+++ app/src/main/java/com/tephra/mc/latestnews/data/model/Article.kt
@@ -7,20 +7,18 @@ import java.util.*
 /**
  * For simplicity this is both the API and ROOM model class for an Article.
  *
- * @param id            Primary Key
+ * @param url           Primary Key - The direct URL to the article
  * @param author        The author of the article
  * @param title         The headline or title of the article
  * @param description   A description or snippet from the article
- * @param url           The direct URL to the article
  * @param urlToImage    The URL to a relevant image for the article
  * @param publishedAt   The date and time that the article was published, in UTC (+000)
  */
 @Entity
-data class Article(@PrimaryKey(autoGenerate = true) var id: Long?,
+data class Article( @PrimaryKey val url: String,
                    val author: String,
                    val title: String,
                    val description: String,
-                   val url: String,
                    val urlToImage: String,
                    val publishedAt: Date
                    )
